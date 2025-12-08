fee79334ee25e4732e7dfaea0bcab49756bfdbec
furkanlale0 <furkanlale409@gmail.com>
2025-12-08 10:49:04 +0300

    acıklama

--- KütüpahaneOtomasyonu/src/main/java/com/example/KutupahaneOtomasyonu/controller/AuthController.java
+++ KütüpahaneOtomasyonu/src/main/java/com/example/KutupahaneOtomasyonu/controller/AuthController.java
@@ -29,7 +29,7 @@ public class AuthController {
         this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
-    }
+    }//deneme
 
     // --- GİRİŞ YAPMA (LOGIN) ---
     @PostMapping("/login")
