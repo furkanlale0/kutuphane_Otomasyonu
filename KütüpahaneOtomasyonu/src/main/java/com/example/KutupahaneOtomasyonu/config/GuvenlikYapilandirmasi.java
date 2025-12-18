598feafa45472c9f63fa79f92e40117538e81924
Furkan Lale <furkanlale408@gmail.com>
2025-12-18 15:35:04 +0300

    deneme için

--- KütüpahaneOtomasyonu/src/main/java/com/example/KutupahaneOtomasyonu/config/GuvenlikYapilandirmasi.java
+++ KütüpahaneOtomasyonu/src/main/java/com/example/KutupahaneOtomasyonu/config/GuvenlikYapilandirmasi.java
@@ -1,5 +1,5 @@
 package com.example.KutupahaneOtomasyonu.config;
-
+//deneme
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
